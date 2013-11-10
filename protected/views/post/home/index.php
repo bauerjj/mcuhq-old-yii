15167b8645ac1198c9a9c26ae93dae5d8bc3a390
Justin Bauer <kapotchy@gmail.com>
2013-11-10 16:58:36 -0700

    Added voting script into its own js file. Changed from yii booster to yiistrap and wheels

--- protected/views/post/home/index.php
+++ protected/views/post/home/index.php
@@ -1,19 +1,22 @@
-<?php
-$this->breadcrumbs = array(
-    '',
-);
-?>
+
 
 <?php
 /* @var $this SiteController */
 
+// No breadcrumbs on main page
+
 $this->pageTitle = Yii::app()->name;
+
+$baseUrl = Yii::app()->baseUrl;
+$cs = Yii::app()->getClientScript();
+$cs->registerScriptFile($baseUrl . '/js/filter-custom.js');
+
 
 
 // Place Navigation
 $this->beginClip('quickLinks');
-$this->widget('bootstrap.widgets.TbMenu', array(
-    'type' => 'list',
+$this->widget('bootstrap.widgets.TbNav', array(
+    'type' => TbHtml::NAV_TYPE_LIST,
     'items' => array(
         array('label' => 'Quick Links'),
         array('label' => 'Get Started', 'icon' => 'icon-play-circle', 'url' => '#', 'active' => false),
@@ -27,7 +30,7 @@ $this->widget('bootstrap.widgets.TbMenu', array(
 $this->endClip();
 ?>
 
-<?php Yii::import('ext.select2.Select2'); ?>
+<?php //Yii::import('ext.select2.Select2'); ?>
 
 <div class="row-fluid">
     <div class="span12 well well-small">
@@ -48,14 +51,14 @@ $this->endClip();
                     foreach ($data as $id => $row) {
                         $options["options"][$id] = array('count' => 6);
                     }
+                    $data = array_merge(array('' => ''), $data);
 
-
-                    $this->widget('ext.select2.ESelect2', array(
+                    $this->widget('yiiwheels.widgets.select2.WhSelect2', array(
                         'attribute' => 'vendor',
                         'name' => 'vendor',
                         'data' => $data, //CHtml::listData(User::model()->findAll(), 'id', 'username'),
-                      //  'htmlOptions' => $options,
-                        'options' => array(
+                        //  'htmlOptions' => $options,
+                        'pluginOptions' => array(
                             'width' => '100%',
                             'height' => '500px',
                             'placeholder' => 'All Vendors',
@@ -84,10 +87,10 @@ $this->endClip();
                 <li><b>Step 2:</b> Select Family<li>
                 <li>
                     <?php
-                    $this->widget('ext.select2.ESelect2', array(
+                    $this->widget('yiiwheels.widgets.select2.WhSelect2', array(
                         'name' => 'family',
-                       // 'data' => $data,
-                        'options' => array(
+                        'data' => array('' => ''),
+                        'pluginOptions' => array(
                             'width' => '100%',
                             'height' => '500px',
                             'placeholder' => 'All Families',
@@ -115,14 +118,16 @@ $this->endClip();
                 <li><b>Step 3: </b> Select Micro <li>
                 <li>
                     <?php
-                    $this->widget('ext.select2.ESelect2', array(
+                    $this->widget('yiiwheels.widgets.select2.WhSelect2', array(
                         'name' => 'micro',
-                       //  'data' => $data,
-                        'options' => array(
+                        'data' => array('' => ''),
+                        'asDropDownList' => true,
+                        'pluginOptions' => array(
                             'width' => '100%',
                             'height' => '500px',
                             'placeholder' => 'All MCUs',
                             'minimumInputLength' => 0,
+                            'allowClear' => true, // Places a small 'x'
                             'formatResult' => 'js:function(data){ return format(data) }',
                         ),
                         'events' => array(
@@ -144,7 +149,11 @@ $this->endClip();
 </div>
 
 <?php
-//CHtml::dropDownList
+echo CHtml::ajaxLink('test me', Yii::app()->createUrl('post/test'), array(
+    'type' => 'POST',
+    'success' => "function(data){ console.log(data)}",
+        )
+);
 ?>
 
 
@@ -156,72 +165,3 @@ $this->widget('bootstrap.widgets.TbListView', array(
     'template' => "{items}\n{pager}" // Puts pager on the bottom
 ));
 ?>
-
-<script>
-    function postData(type) {
-       // alert($('#vendor').select2('val') + ', ' + $('#family').select2('val') + ', ' + $('#micro').select2('val'));
-
-        $.ajax({
-            url: "<?php echo Yii::app()->createUrl('post/filter') ?>",
-            type: 'POST',
-            data: {'type': type, 'vendorId': $('#vendor').val(), 'familyId': $('#family').val(), 'microId': $('#micro').val()},
-            success: function(data) {
-                var data = $.parseJSON(data); // Parse the JSON from controller
-
-                $("#family").html(data.family);
-                $("#micro").html(data.micro);
-
-                if (type === 'vendor') {
-                    $("#select-family .select2-container .select2-choice abbr").removeAttr('style');
-
-                    if($("#s2id_family a").hasClass('select2-default'))
-                        $("#select-family .select2-container .select2-choice abbr").hide();
-                }
-                else if(type === 'family'){
-                    $("#select-micro .select2-container .select2-choice abbr").removeAttr('style');
-
-                     if($("#s2id_micro a").hasClass('select2-default'))
-                        $("#select-micro .select2-container .select2-choice abbr").hide();
-                }
-                // this enables the little cross to appear on the micro dropdown
-                else if(type === 'micro'){
-                    $("#select-micro .select2-container .select2-choice abbr").removeAttr('style');
-
-                     if($("#s2id_micro a").hasClass('select2-default'))
-                        $("#select-micro .select2-container .select2-choice abbr").hide();
-                }
-
-
-
-
-                if (type === 'vendor-removed'){
-                    $("#select-family a").addClass('select2-default');
-                    $("#select-family a span").html('All Families');
-                }
-                if (type === 'family-removed' || type === 'vendor-removed'){
-                    $("#select-micro a").addClass('select2-default');
-                    $("#select-micro a span").html('All MCUs');
-                }
-            },
-            beforeSend: function() {
-                if (type === 'vendor') {
-                    $("#select-family .select2-container .select2-choice abbr").show();
-                    $("#select-family .select2-container .select2-choice abbr").css("background", "url('assets/b9896ca8/select2-spinner.gif') no-repeat 100%, -moz-linear-gradient(center bottom, white 85%, #eeeeee 99%)");
-                }
-                else if (type === 'family') {
-                    $("#select-micro .select2-container .select2-choice abbr").show();
-                    $("#select-micro .select2-container .select2-choice abbr").css("background", "url('assets/b9896ca8/select2-spinner.gif') no-repeat 100%, -moz-linear-gradient(center bottom, white 85%, #eeeeee 99%)");
-                }
-            },
-        });
-    }
-
-    ;
-
-    function format(data) {
-       //  console.log(data);
-        // Using a custom formatResult will omit the underline under the matching characters
-        return '<span class="label" style="margin-right: 15px" >5</span>' + data.text;
-    }
-
-</script>
